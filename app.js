118c2b4876cdbd19cd0fcb72c30d994cb2f58023
Clifford Musyoka <your_email@example.com>
2024-05-23 22:36:06 +0300

    Calculate Duration Left

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ const giveaway = document.querySelector(".giveaway");
 const deadline = document.querySelector(".deadline");
 const items = document.querySelectorAll(".deadline-format h4");
 
-let futureDate = new Date(2027, 10, 21, 11, 30, 0);
+let futureDate = new Date(2024, 10, 21, 11, 30, 0);
 
 const year = futureDate.getFullYear();
 const hours = futureDate.getHours();
@@ -39,3 +39,31 @@ let day = weekdays[futureDate.getDay()];
 
 giveaway.textContent = `Giveaway ends on ${day}, ${date}, ${month},${year}, ${hours}: ${min}am `
 
+//future time in ms
+const futureTime = futureDate.getTime();
+
+function getRemainingTime(){
+  const today = new Date().getTime();
+  const t = futureTime - today;
+
+  //value in ms
+  const oneDay = 24 * 60 * 60 * 1000;
+  const oneHour = 60 * 60 * 1000;
+  const oneMinute = 60 * 1000;
+
+  //calculate all values
+  let days = t / oneDay;
+  days = Math.floor(days);
+  let hours =  Math.floor((t % oneDay) / oneHour);
+  let minutes = Math.floor((t % oneHour) / oneMinute);
+  let seconds = Math.floor((t % oneMinute) / 1000);
+
+  //set values Array
+  const values = [days,hours,minutes,seconds];
+
+  items.forEach(function(item, index) {
+    item.innerHTML = values[index];
+  })
+}
+
+getRemainingTime();
